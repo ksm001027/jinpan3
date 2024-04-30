3bcf551c714930f056095f29df33ec597bd80aee
고승민 <ksm001027@naver.com>
2024-04-30 20:34:38 +0900

    mysql

--- src/main/java/com/example/react/ReactApplication.java
+++ src/main/java/com/example/react/ReactApplication.java
@@ -6,14 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactApplication {
 
-<<<<<<< HEAD
     public static void main(String[] args) {
         SpringApplication.run(ReactApplication.class, args);
     }
-=======
-	public static void main(String[] args) {
-		SpringApplication.run(ReactApplication.class, args);
-	}
->>>>>>> f8eb17273d2b865216b158b8f613e0a5d9019879
 
 }
